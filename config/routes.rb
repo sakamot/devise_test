fa1ddbad50a3d473248c0419090d40d49ffe1aa3
sakamot <matuhana.8@gmail.com>
2018-01-28 23:06:23 +0900

    fix commit leakage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
+  post 'api/users/sign_up', to: 'api/users#sign_up'
+
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: '/letter_opener'
